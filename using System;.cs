8d4daa57d6abf0531dcccaebaf70f1916b475774
YashD45 <96069752+YashD45@users.noreply.github.com>
2021-12-14 22:44:06 +0530

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+
+namespace Hello world
+{
+   class program
+   {
+       static void Main(string[] args)
+{
+    Console.WriteLine("Hello World");
+}
+   }
+}
